049994bc2b2d66df70b4aff6d9655f0923ceaa8b
chutipong roobklom <khunemz@gmail.com>
2019-10-28 22:58:41 +0700

    add viewmodel

---
+++ FriendOrganize.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using FriendOrganize.Model;
+using FriendOrganize.UI.Data;
+
+namespace FriendOrganize.UI.ViewModels
+{
+	internal class MainViewModel : ViewModelBase
+	{
+		private readonly IFriendDataService _friendDataService;
+
+		private Friend _selectedFriend;
+
+		public MainViewModel(IFriendDataService friendDataService)
+		{
+			Friends = new ObservableCollection<Friend>();
+
+			_friendDataService = friendDataService;
+		}
+
+		public ObservableCollection<Friend> Friends { get; set; }
+
+
+		public Friend SelectedFriend
+		{
+			get { return _selectedFriend }
+			set
+			{
+				_selectedFriend = value;
+				OnPropertyChanged(nameof(SelectedFriend));
+			}
+		}
+
+		public void Load()
+		{
+			var friends = _friendDataService.GetAll();
+
+			Friends.Clear();
+			foreach (var friend in friends) Friends.Add(friend);
+		}
+	}
+}
